eddd802dddf3503619fda90adcde15c656b873ee
Arslancann <arslanaltun24@gmail.com>
2021-01-12 01:51:17 +0300

    basit hesap makinesi eklendi

--- src/BasitHesapMakinesi.java
+++ src/BasitHesapMakinesi.java
@@ -1,48 +1,50 @@
-package ortalamahesaplama;
-
 import java.util.Scanner;
 
-public class SoruCevap1 {
+public class BasitHesapMakinesi {
     
     public static void main(String[]  args)
     {
-        
         Scanner scanner1 = new Scanner(System.in);
+        
+        //birinci sayıyı kullanıcıdan al
         System.out.print("1.sayıyı giriniz: ");
         int sayi1 = scanner1.nextInt();
         
-        Scanner scanner2 = new Scanner(System.in);
+        //ikinci sayıyı kullanıcıdan al
         System.out.print("2.sayıyı giriniz: ");
-        int sayi2 = scanner2.nextInt();
+        int sayi2 = scanner1.nextInt();
         
-        Scanner islem = new Scanner(System.in);
+        //işlem türünü kullanıcıdan al
+        scanner1.nextLine();
         System.out.print("İşlem Türü(+ - * /): ");
-        String tercih = islem.nextLine();
+        String tercih = scanner1.nextLine();
         
-        if(tercih.equals("+"))
+        
+        if(tercih.equals("+")) //işlem türünü kontrol et
         {
-            int toplam = sayi1 + sayi2;
-            System.out.println("Sonuç: " + toplam);
+            int toplam = sayi1 + sayi2; //sonucu hesapla
+            System.out.println("Sonuç: " + toplam); //sonucu yazdır
         }
         
-        else if(tercih.equals("-"))
+        else if(tercih.equals("-")) //işlem türünü kontrol et
         {
-            int cıkar = sayi1 - sayi2;
-            System.out.println("Sonuç: " + cıkar);
-                              
-        }
-        else if(tercih.equals("*"))
-        {
-            int cıkar = sayi1 * sayi2;
-            System.out.println("Sonuç: " + cıkar);
+            int cıkar = sayi1 - sayi2; //sonucu hesapla
+            System.out.println("Sonuç: " + cıkar); //sonucu yazdır                  
         }
         
-        else if(tercih.equals("/"))
+        else if(tercih.equals("*")) //işlem türünü kontrol et
         {
-            int bol = sayi1 / sayi2;
-            System.out.println("Sonuç: " + bol);
+            int cıkar = sayi1 * sayi2; //sonucu hesapla
+            System.out.println("Sonuç: " + cıkar); //sonucu yazdır
         }
         
+        else if(tercih.equals("/")) //işlem türünü kontrol et
+        {
+            int bol = sayi1 / sayi2; //sonucu hesapla
+            System.out.println("Sonuç: " + bol); //sonucu yazdır
+        }
+        
+        //işlem türünü kontrol et, farklı ise hata mesajı yazdır
         else
         {
             System.out.println("Lütfen Seçim Yapınız!!!");
